beb52467c87bf7b0594b6fcf8f1531bb92286c3d
Arditti93 <alex.arditti@hotmail.com>
2022-11-30 10:31:26 +0000

    added add invoice and find user invoices

--- index.js
+++ index.js
@@ -92,58 +92,64 @@ const displayTables = async () => {
     }
 }
 
-// displayTables()
+displayTables()
 
-//The Invoices table stores the userId of the user who created the invoice row.
-//id is the primary key in the users table and the forigen key in invoices table 
 
-//id is called userId in the invocies table
-
-//The first method to query both table data at once is by writing a raw SQL query using
 const rawJoin = async () => {
-    const [results] = await sequelize.query(
+    const [results] = await sequelize.query (
         "SELECT * FROM Invoices JOIN Users ON Invoices.userId = Users.id"
     )
     console.table(results.map(value => value))
-} 
+}
+// rawJoin()
 
-rawJoin()
+///////////////////////////////////
 
-// There are four types of association methods that you can use in Sequelize:
-// hasOne()
-// hasMany()
-// belongsTo()
-// belongsToMany()
-// These four methods can be used to create One-To-One, One-To-Many, and Many-To-Many relationships between your models. 
-
-// In the example we have, one User row can have many Invoices rows. This means you need to create a One-To-Many relation between the Users and the Invoices table.
-
-const sequelizeJoin = async () => { 
+const sequelizeJoin = async () => {
     User.hasMany(Invoice)
     Invoice.belongsTo(User)
 
-    // By default, the include option will cause Sequelize to generate an SQL query with the LEFT OUTER JOIN clause.
-    const leftOuterJoin = await User.findAll({
-        include: Invoice
-    });
+    // const leftOuterJoin = await User.findAll({
+    //     include: Invoice
+    // })
 
-    console.log(JSON.stringify(leftOuterJoin, null, 2)) 
+    // console.log(JSON.stringify(leftOuterJoin, null, 2)) 
 
-    //To change the JOIN clause from LEFT OUTER JOIN to INNER JOIN, 
-    //you need to add the required: true option to the include option.
 
     const innerJoin = await User.findAll({
-        include: { model: Invoice, required: true },
-      });
-      
-    //With an INNER JOIN, the data from the Users row will 
-    //be excluded when there are no related Invoices data for that row.
-    console.log(JSON.stringify(innerJoin, null, 2));
+        include: {model: Invoice, required: true }
+    })
+
+    console.log(JSON.stringify(innerJoin, null, 2))
 }
+//  sequelizeJoin()
 
-sequelizeJoin()
+//add invoice for a user called Alice 
+const addInvoice = async () => {
+    //find userid of user
+    let userId = await User.findAll({
+        where: {
+            firstName: 'Alice'
+        },
+        attributes: ['id']
+    })
 
+    let id = userId[0].dataValues.id
+    await Invoice.create({
+        amount: 400,
+        userId: id
+    })
+    console.log("Invoice successfully created") 
 
+    let findInvoice = await Invoice.findAll({
+        where: {
+            userId: id
+        }
+    })
+    console.table(findInvoice.map(value => value.dataValues)) 
+
+}
+addInvoice() 
 
 
 
